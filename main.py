6b5654bc7ed3dd6be42f19eac22d095c9f1c27ae
srisha p <srishapyati4@gmail.com>
2021-03-21 06:11:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#data types
+int
+str
+bool
+float
+list
+tuple
+set
+dict
+
+print(3+4)#interger
+print(2.1+3)#float
+
+
+print(5 % 40)#modulo
+print(3 ** 2)#power
+print(3//2)#roundoff
+
+
+#math functions
+print(round(3.9))
+print(abs(2-3))# acts as mod
